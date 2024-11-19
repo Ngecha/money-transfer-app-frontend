f8ab68cec9cf3badadb13b4554dfa2324249e15e
Ngecha <ngechadennis@gmail.com>
2024-11-19 05:35:40 +0300

    changes

--- src/Components/SignUpModal.js
+++ src/Components/SignUpModal.js
@@ -27,7 +27,7 @@ const SignUpForm = ({ onClose, onOpenLogin }) => {
     onSubmit: async (values) => {
       setIsLoading(true);
       try {
-        const response = await fetch("http://127.0.0.1:5000/signup", {
+        const response = await fetch("http://127.0.0.1:5000/register", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(values),
